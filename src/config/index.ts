505707577de0039a1d86dee600b4abc59517042a
thguss <thgus345@naver.com>
2023-01-02 20:21:11 +0900

    [ADD] env 연결 config 추가

---
+++ src/config/index.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+export default {
+  /**
+   * Your favorite port
+   */
+  port: parseInt(process.env.PORT as string, 10) as number,
+
+  /**
+   * AWS S3
+   */
+  s3AccessKey: process.env.S3_ACCESS_KEY as string,
+  s3SecretKey: process.env.S3_SECRET_KEY as string,
+  bucketName: process.env.BUCKET_NAME as string,
+
+  /**
+   * JWT
+   */
+  jwtSecret: process.env.JWT_SECRET as string,
+  jwtAlgo: process.env.JWT_ALGORITHM as string,
+};
