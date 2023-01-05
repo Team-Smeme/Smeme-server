08850943a01179a0c30eadca4a39430e74d415bd
thguss <thgus345@naver.com>
2023-01-06 04:58:44 +0900

    [DEL] 미사용 주석 삭제

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,17 @@
-// src/index.ts
 import express, { Request, Response } from "express";
 import config from "./config";
+import router from "./routers";
 
-const app = express(); // express 객체 받아옴
-const PORT = config.port; // 사용할 port를 3000번으로 설정
+const app = express();
+const PORT = config.port;
 
-app.use(express.json()); // express 에서 request body를 json 으로 받아오겠다.
+app.use(express.json());
 
-// app.use("/api", require("./api")); // use -> 모든 요청
-// localhost:8000/api -> api 폴더
-// localhost:8000/api/user -> user.ts
+app.use("/api/v1", router);
 
-//* HTTP method - GET
 app.get("/", (req: Request, res: Response) => {
   res.send("서버 연결");
 });
-
-/** next : 미들웨어 사용
- app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("서버 연결");
-});
-
- */
 
 app.listen(PORT, () => {
   console.log(`
@@ -29,4 +19,6 @@ app.listen(PORT, () => {
             🛡️ Server listening on port: ${PORT} 🛡️
         #############################################
     `);
-}); // 8000 번 포트에서 서버를 실행하겠다!
+});
+
+export default app;
