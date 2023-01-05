d0b8a8e52ff79d64faae294367a046d10f839faa
unanchoi <unusualc@likelion.org>
2023-01-05 22:23:20 +0900

    [ADD] reponse util 추가

---
+++ src/utils/response.ts
@@ -0,0 +1,15 @@
+export const success = (status: number, message: string, data?: any) => {
+  return {
+    status,
+    success: true,
+    message,
+    data,
+  };
+};
+export const fail = (status: number, message: string) => {
+  return {
+    status,
+    success: false,
+    message,
+  };
+};
