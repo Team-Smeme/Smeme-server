908e2d391164d73b0e49bcc82bfe6bd019de3433
unanchoi <unusualc@likelion.org>
2023-01-06 17:46:54 +0900

    [FEAT] 내일기 상세조회 router 추가

--- src/routers/UserRouter.ts
+++ src/routers/UserRouter.ts
@@ -4,5 +4,6 @@ import { userController } from "../controllers";
 const router: Router = Router();
 
 router.patch("/", userController.updateUserInfo);
+router.get("/diaries/:diaryId", userController.getUserDiaryDetail);
 
 export default router;
