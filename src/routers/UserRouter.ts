98d2eebc8ff3ca3167a29bb3e5f05fa183a70cb2
unanchoi <unusualc@likelion.org>
2023-01-07 15:19:53 +0900

    [CHORE] 유저정보 설정 API validation 추가

--- src/routers/UserRouter.ts
+++ src/routers/UserRouter.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { UserController } from "../controllers";
-
+import { body } from "express-validator";
 const router: Router = Router();
 
-router.patch("/", UserController.updateUserInfo);
+router.patch(
+  "/",
+  [
+    body("username").notEmpty().isLength({ min: 2, max: 16 }),
+    body("bio").notEmpty(),
+  ],
+  UserController.updateUserInfo,
+);
 router.get("/diaries/:diaryId", UserController.getUserDiaryDetail);
 router.get("/diaries", UserController.getUserDiaryList);
 router.get("/", UserController.getUserInfo);
