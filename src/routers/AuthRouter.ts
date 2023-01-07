a3233067c36b96115e7d4ad226b974f7bf31c749
unanchoi <unusualc@likelion.org>
2023-01-07 15:39:04 +0900

    [CHORE] 소셜로그인 API validation 추가

--- src/routers/AuthRouter.ts
+++ src/routers/AuthRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { AuthController } from "../controllers";
+import { body } from "express-validator";
 
 const router: Router = Router();
 
-router.post("/", AuthController.signIn);
+router.post("/", [body("social").notEmpty()], AuthController.signIn);
 router.get("/token", AuthController.getToken);
 
 export default router;
