8c5a8b024efbd2528c4f23c73ecbfee5366d8ec4
thguss <thgus345@naver.com>
2023-01-07 14:55:17 +0900

    [ADD] .env 파일 deploy 파일 반영

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,13 +11,6 @@ export default {
   port: parseInt(process.env.PORT as string, 10) as number,
 
   /**
-   * AWS S3
-   */
-  // s3AccessKey: process.env.S3_ACCESS_KEY as string,
-  // s3SecretKey: process.env.S3_SECRET_KEY as string,
-  // bucketName: process.env.BUCKET_NAME as string,
-
-  /**
    * JWT
    */
   jwtSecret: process.env.JWT_SECRET as string,
