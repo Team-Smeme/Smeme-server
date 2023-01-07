e744767762ef9972b4fe62a5cdab79fe3fa337e4
unanchoi <unusualc@likelion.org>
2023-01-07 16:03:44 +0900

    [CHORE] AuthRouter [] 제거

--- src/routers/AuthRouter.ts
+++ src/routers/AuthRouter.ts
@@ -4,7 +4,7 @@ import { body } from "express-validator";
 
 const router: Router = Router();
 
-router.post("/", [body("social").notEmpty()], AuthController.signIn);
+router.post("/", body("social").notEmpty(), AuthController.signIn);
 router.get("/token", AuthController.getToken);
 
 export default router;
