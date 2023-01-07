59b6aa924027435bc29f2f75697f62317d2e470b
thguss <thgus345@naver.com>
2023-01-07 16:40:01 +0900

    [FIX] develop 버전 병합

--- src/services/DiaryService.ts
+++ src/services/DiaryService.ts
@@ -1,16 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+import dayjs from "dayjs";
+import { status } from "../constants";
 import {
+  DiaryRequestDto,
   DiaryDeleteRequestDto,
   DiaryUpdateRequestDto,
 } from "./../interfaces/diary/DiaryRequestDto";
-import { PrismaClient } from "@prisma/client";
-import { DiaryRequestDto } from "../interfaces/diary/DiaryRequestDto";
-import dayjs from "dayjs";
-import { status } from "../constants";
 import {
   DiaryResponseDto,
   OpenDiaryResponseDto,
 } from "../interfaces/diary/DiaryResponseDto";
-import statusCode from "../constants/statusCode";
+import { DiaryLikeDto } from "../interfaces/diary/DiaryLikeDto";
 
 const prisma = new PrismaClient();
 
@@ -53,6 +53,28 @@ const createDiary = async (diaryRequestDto: DiaryRequestDto) => {
   };
 };
 
+const deleteDiary = async (diaryDeleteRequestDto: DiaryDeleteRequestDto) => {
+  const { userId, diaryId } = diaryDeleteRequestDto;
+
+  const user = await prisma.users.findUnique({
+    where: {
+      id: +userId,
+    },
+  });
+
+  if (!user) {
+    return status.UNAUTHORIZED;
+  }
+
+  const data = await prisma.diaries.delete({
+    where: {
+      id: +diaryId,
+    },
+  });
+
+  return data;
+};
+
 const getDiaryById = async (diaryId: number, userId: number) => {
   const user = await prisma.users.findUnique({
     where: {
@@ -61,7 +83,7 @@ const getDiaryById = async (diaryId: number, userId: number) => {
   });
 
   if (!user) {
-    return statusCode.UNAUTHORIZED;
+    return status.UNAUTHORIZED;
   }
 
   const diary = await prisma.diaries.findUnique({
@@ -91,7 +113,7 @@ const getDiaryById = async (diaryId: number, userId: number) => {
   });
 
   if (!category) {
-    return statusCode.INTERNAL_SERVER_ERROR;
+    return status.INTERNAL_SERVER_ERROR;
   }
 
   const likeCnt = await prisma.likes.count({
@@ -117,7 +139,7 @@ const getDiaryById = async (diaryId: number, userId: number) => {
   });
 
   if (!writer) {
-    return statusCode.INTERNAL_SERVER_ERROR;
+    return status.INTERNAL_SERVER_ERROR;
   }
 
   const date = dayjs(diary.created_at).format("YYYY-MM-DD HH:mm");
@@ -146,7 +168,7 @@ const getOpenDiaries = async (userId: number) => {
   });
 
   if (!user) {
-    return statusCode.UNAUTHORIZED;
+    return status.UNAUTHORIZED;
   }
 
   let diaries = await prisma.diaries.findMany();
@@ -188,7 +210,7 @@ const getOpenDiaries = async (userId: number) => {
     });
 
     if (!writer) {
-      return statusCode.INTERNAL_SERVER_ERROR;
+      return status.INTERNAL_SERVER_ERROR;
     }
 
     const data: OpenDiaryResponseDto = {
@@ -212,28 +234,6 @@ const getOpenDiaries = async (userId: number) => {
   });
 
   return result;
-};
-
-const deleteDiary = async (diaryDeleteRequestDto: DiaryDeleteRequestDto) => {
-  const { userId, diaryId } = diaryDeleteRequestDto;
-
-  const user = await prisma.users.findUnique({
-    where: {
-      id: +userId,
-    },
-  });
-
-  if (!user) {
-    return status.UNAUTHORIZED;
-  }
-
-  const data = await prisma.diaries.delete({
-    where: {
-      id: +diaryId,
-    },
-  });
-
-  return data;
 };
 
 const updateDiary = async (diaryUpdateRequestDto: DiaryUpdateRequestDto) => {
@@ -292,12 +292,69 @@ const updateDiary = async (diaryUpdateRequestDto: DiaryUpdateRequestDto) => {
   return result;
 };
 
+const getLikeDiary = async (userId: number, diaryId: number) => {
+  const user = await prisma.users.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!user) {
+    return status.UNAUTHORIZED;
+  }
+
+  const diary = await prisma.diaries.findUnique({
+    where: {
+      id: diaryId,
+    },
+  });
+
+  if (!diary) {
+    return status.BAD_REQUEST;
+  }
+
+  let hasLike =
+    (await prisma.likes.count({
+      where: {
+        user_id: userId,
+        diary_id: diaryId,
+      },
+    })) > 0
+      ? true
+      : false;
+
+  if (hasLike) {
+    await prisma.likes.deleteMany({
+      where: {
+        user_id: userId,
+        diary_id: diaryId,
+      },
+    });
+    hasLike = false;
+  } else {
+    await prisma.likes.create({
+      data: {
+        user_id: userId,
+        diary_id: diaryId,
+      },
+    });
+    hasLike = true;
+  }
+
+  const data: DiaryLikeDto = {
+    hasLike: hasLike,
+  };
+
+  return data;
+};
+
 const diaryService = {
   createDiary,
+  deleteDiary,
   getDiaryById,
   getOpenDiaries,
-  deleteDiary,
   updateDiary,
+  getLikeDiary,
 };
 
 export default diaryService;
