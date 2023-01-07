8816778690ed27cc9e2b251af48b50084473521b
thguss <thgus345@naver.com>
2023-01-07 14:56:00 +0900

    [DEL] 미사용 코드 삭제

--- src/routers/TestRouter.ts
+++ src/routers/TestRouter.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
-import { Response } from "express";
 import testController from "../controllers/TestController";
-import { success } from "../utils/response";
 
 const router: Router = Router();
 
