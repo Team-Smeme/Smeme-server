67c000b395f80f5a82ed30e2716708fc22f3d515
unanchoi <unusualc@likelion.org>
2023-01-08 13:43:03 +0900

    [CHORE] header express-validator 추가

--- src/routers/AuthRouter.ts
+++ src/routers/AuthRouter.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { AuthController } from "../controllers";
-import { body } from "express-validator";
+import { body, header } from "express-validator";
 
 const router: Router = Router();
 
-router.post("/", body("social").notEmpty(), AuthController.signIn);
+router.post(
+  "/",
+  [body("social").notEmpty(), header("authorization").notEmpty()],
+  AuthController.signIn,
+);
 router.get("/token", AuthController.getToken);
 
 export default router;
