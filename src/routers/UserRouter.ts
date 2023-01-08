34c048ad53338ce0ba9371eeacd495c5b0c249af
unanchoi <unusualc@likelion.org>
2023-01-08 15:33:09 +0900

    [CHORE] 유저 일기 상세 조회 라우터 auth 미들웨어 추가

--- src/routers/UserRouter.ts
+++ src/routers/UserRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { UserController } from "../controllers";
 import { body } from "express-validator";
+import auth from "../middlewares/auth";
 const router: Router = Router();
 
 router.patch(
@@ -11,7 +12,7 @@ router.patch(
   ],
   UserController.updateUserInfo,
 );
-router.get("/diaries/:diaryId", UserController.getUserDiaryDetail);
+router.get("/diaries/:diaryId", auth, UserController.getUserDiaryDetail);
 router.get("/diaries", UserController.getUserDiaryList);
 router.get("/", UserController.getUserInfo);
 
