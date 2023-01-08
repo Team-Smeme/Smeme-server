a4fd96baa77b23a3505fb2b33c4a1906f3cf0623
unanchoi <unusualc@likelion.org>
2023-01-08 15:35:37 +0900

    [CHORE] 스크랩 저장 auth 미들웨어 추가

--- src/routers/ScrapRouter.ts
+++ src/routers/ScrapRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { body } from "express-validator";
 import { ScrapController } from "../controllers";
+import auth from "../middlewares/auth";
 
 const router: Router = Router();
 
 router.post(
   "/",
+  auth,
   [body("diaryId").notEmpty(), body("paragraph").notEmpty()],
   ScrapController.createScrap,
 );
