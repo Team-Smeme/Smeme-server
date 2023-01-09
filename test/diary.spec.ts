5b277177ebeb497ecf059ae5cb4865f0ce9ae109
thguss <thgus345@naver.com>
2023-01-10 00:23:06 +0900

    [FEAT] 일기 테스트 추가

---
+++ test/diary.spec.ts
@@ -0,0 +1,102 @@
+import path from "path";
+import { expect } from "chai";
+import app from "../src/index";
+import req from "supertest";
+import dotenv from "dotenv";
+import config from "../src/config";
+
+dotenv.config();
+
+describe("Diary Test", () => {
+  let diaryId = -1;
+
+  it("일기 작성 성공", (done) => {
+    req(app)
+      .post("/api/v1/diaries")
+      .set("Content-Type", "application/json")
+      .set({ Authorization: `Bearer ${config.testAccessToken}` })
+      .send({
+        content: "Hello, I'm a server developer of the smeme!",
+        targetLang: "en",
+        topicId: 0,
+        isPublic: true,
+      })
+      .expect(201)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        diaryId = res.body.data.diaryId;
+        done();
+      })
+      .catch((err) => {
+        console.error("=== Error === \n", err);
+        done(err);
+      });
+  });
+
+  it("게시판 조회 성공", (done) => {
+    req(app)
+      .get("/api/v1/diaries")
+      .set("Content-Type", "application/json")
+      .set({ Authorization: `Bearer ${config.testAccessToken}` })
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        console.error("=== Error === \n", err);
+        done(err);
+      });
+  });
+
+  it("게시판 일기 상세 조회 성공", (done) => {
+    req(app)
+      .get(`/api/v1/diaries/${diaryId}`)
+      .set("Content-Type", "application/json")
+      .set({ Authorization: `Bearer ${config.testAccessToken}` })
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        console.error("=== Error === \n", err);
+        done(err);
+      });
+  });
+
+  it("일기 좋아요 성공", (done) => {
+    req(app)
+      .post("/api/v1/diaries/like")
+      .set("Content-Type", "application/json")
+      .set({ Authorization: `Bearer ${config.testAccessToken}` })
+      .send({
+        diaryId: diaryId,
+      })
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        console.error("=== Error === \n", err);
+        done(err);
+      });
+  });
+
+  it("게시판 삭제 성공", (done) => {
+    req(app)
+      .delete(`/api/v1/diaries/${diaryId}`)
+      .set("Content-Type", "application/json")
+      .set({ Authorization: `Bearer ${config.testAccessToken}` })
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        console.error("=== Error === \n", err);
+        done(err);
+      });
+  });
+});
