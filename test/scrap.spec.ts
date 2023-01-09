1b4fed241b929d62dad6b70b09c4ee59a41b4f77
thguss <thgus345@naver.com>
2023-01-10 02:57:49 +0900

    [FEAT] 스크랩 테스트 코드 기능 구현

--- test/scrap.spec.ts
+++ test/scrap.spec.ts
@@ -9,7 +9,6 @@ dotenv.config();
 let scrapId = -1;
 
 describe("Scrap Test", () => {
-  console.log(diary.diaryId);
   it("구문 스크랩 성공", (done) => {
     req(app)
       .post("/api/v1/scraps")
@@ -23,6 +22,38 @@ describe("Scrap Test", () => {
       .expect("Content-Type", /json/)
       .then((res) => {
         scrapId = res.body.data.scrapId;
+        done();
+      })
+      .catch((err) => {
+        console.error("=== Error === \n", err);
+        done(err);
+      });
+  });
+
+  it("구문 스크랩 리스트 조회 성공", (done) => {
+    req(app)
+      .get("/api/v1/scraps")
+      .set("Content-Type", "application/json")
+      .set({ Authorization: `Bearer ${config.testAccessToken}` })
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        console.error("=== Error === \n", err);
+        done(err);
+      });
+  });
+
+  it("구문 스크랩 삭제 성공", (done) => {
+    req(app)
+      .delete(`/api/v1/scraps/${scrapId}`)
+      .set("Content-Type", "application/json")
+      .set({ Authorization: `Bearer ${config.testAccessToken}` })
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then(() => {
         done();
       })
       .catch((err) => {
